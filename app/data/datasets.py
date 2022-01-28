9ed3937e92424dc0a93a78568ecb719f95c4f108
Omar Ali Sheikh <sheikhomar@users.noreply.github.com>
2022-01-28 15:41:41 +0100

    Noise generation.

--- app/data/datasets.py
+++ app/data/datasets.py
@@ -6,6 +6,7 @@ from typing import Dict, Generator, List, Set
 import pandas as pd
 import numpy as np
 
+from cleanlab.noise_generation import generate_noisy_labels
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.model_selection import StratifiedKFold
 from sklearn.preprocessing import LabelEncoder
@@ -74,6 +75,10 @@ class IceCatOfficeDataSet(DataSet):
         large_enough_categories = category_counts[category_counts >= min_num_samples_per_category].index.tolist()
         self._df_data = df_data[df_data.category_name.isin(large_enough_categories)]
 
+        # Save a copy of the original labels for each sample.
+        # This allows us to apply multiple label noise matrices.
+        self._original_labels = list(self._df_data[self.label_attribute])
+
         # Encode labels
         self._label_encoder = LabelEncoder()
         self._label_encoder.fit(self._df_data[self.label_attribute])
@@ -126,7 +131,27 @@ class IceCatOfficeDataSet(DataSet):
                 X_test=X_test,
                 y_test=y_test
             )
-      
+
+    def apply_label_noise(self, noise_matrix: np.ndarray) -> None:
+        # Reload original labels before applying a new noise matrix.
+        # This resets earlier noise matrices.
+        self._df_data[self.label_attribute] = self._original_labels
+        y_original = self._label_encoder.transform(self._original_labels)
+
+        # Generate noisy labels from the labels.
+        y_modified = generate_noisy_labels(y=y_original, noise_matrix=noise_matrix)
+        
+        # Determine which sample indices have been modified.
+        modified_indices, = np.where(y_modified != y_original)
+
+        # Modify the internal dataframe.
+        new_labels = [self._label_encoder.classes_[l] for l in y_modified]
+        self._df_data[self.label_attribute] = new_labels
+
+        # Transform to label indices.
+        self._y_given = self._label_encoder.transform(self._df_data[self.label_attribute])
+
+        return modified_indices       
 
 
 def get_dataset(file_path: str) -> DataSet:
